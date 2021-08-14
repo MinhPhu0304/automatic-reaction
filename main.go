a5e4ec2eb13c1aac810bc0a3132986bb7ca0726f
minh <Lminhphu0304@gmail.com>
2021-08-14 13:51:13 +1200

    Attach sentry hub to gin context

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
-	"github.com/Minhphu0304/slack-reaction-bot/handler"
+	"github.com/getsentry/sentry-go"
+	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+
+	"github.com/Minhphu0304/slack-reaction-bot/handler"
 )
 
 func main() {
@@ -15,10 +19,15 @@ func main() {
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn: os.Getenv("SENTRY_DSN"),
+	}); err != nil {
+		fmt.Printf("Sentry initialization failed: %v\n", err)
+	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
-
+	router.Use(sentrygin.New(sentrygin.Options{}))
 	router.GET("/", handler.HandleRootRequest)
 
 	router.POST("/reaction-bot", handler.ReactionBot)
